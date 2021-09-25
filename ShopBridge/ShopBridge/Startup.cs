db24562157b2f699251e05b02f554d93d44ff278
Pradeep Pandit <pradeep.pandit@tietoevry.com>
2021-09-26 00:22:37 +0530

    Added documentation for ShopBridge e-commerce web api

--- ShopBridge/ShopBridge/Startup.cs
+++ ShopBridge/ShopBridge/Startup.cs
@@ -1,23 +1,15 @@
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.Identity.Web;
 using Microsoft.OpenApi.Models;
 using ShopBridge.Entities;
 using ShopBridge.Models;
 using ShopBridge.Repository;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ShopBridge
 {
